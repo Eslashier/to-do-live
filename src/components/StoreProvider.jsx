521a5277bbec5a25bf6ff878020bfc5c19c4c2d5
Eslashier <anfelov@gmail.com>
2022-05-03 21:42:24 -0500

    Delete notes from the database

--- src/components/StoreProvider.jsx
+++ src/components/StoreProvider.jsx
@@ -9,12 +9,6 @@ const initialState = {
         done:false
     },
     listOfNotes:[
-        {
-            id: 0,
-            title:'Title by default',
-            message:'message by default',
-            done:true
-        },
     ]
 }
 
